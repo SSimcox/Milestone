39255570f3dfd22d52eb1cc00c3876c4def4d04f
IssacImmarel <steven.d.simcox@gmail.com>
2016-10-11 18:10:19 -0600

    Add submit behavior, form validation

---
+++ js/contact.js
@@ -0,0 +1,36 @@
+/**
+ * Created by Steven on 10/11/2016.
+ */
+
+var emailRegex = /^[\w-]+(?:\.[\w-]+)*@(?:[\w-]+\.)+[a-zA-Z]{2,7}$/;
+
+function formValidate(){
+    var validEmail = emailRegex.exec($('#email').val());
+    if(validEmail != undefined){
+        if($('#email').val() === $('#confirm').val()) {
+            return true;
+        }
+        else{
+            $('#confirm').val("");
+            $('#confirm').attr('placeholder',"The email addresses does not match");
+            return false;
+        }
+    }
+    else{
+        $('#email').val("");
+        $('#email').attr('placeholder',"That is not a valid email address");
+        return false;
+    }
+    return false;
+}
+
+$(document).ready(function(){
+    $('#submit').on("click", function(){
+            $('#contact-form').submit(function(){
+                var valid = formValidate();
+                $('#contact-form').unbind('submit').bind('submit');
+                return valid;
+            });
+    });
+
+});
